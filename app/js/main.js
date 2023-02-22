9305b4f532cdb6055dc68283e6a32ef1eed9cfc4
PerpetuumG <perpetuumg@gmail.com>
2023-02-22 13:17:41 +0200

    adaptive header

--- app/js/main.js
+++ app/js/main.js
@@ -92,4 +92,9 @@ $(function () {
   $('.surfboard-box__circle').on('click', function () {
     $(this).toggleClass('active');
   });
+
+  //
+  $('.menu-btn').on('click', function () {
+    $('.menu').toggleClass('active');
+  });
 });
